778749543a7ac07565a7425dda0cf57b593bf4f7
Striletska Kateryna <striletskay2005@gmail.com>
2024-10-23 15:42:51 +0300

    Update cargo.toml, read.me and fix bug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use my_parser::list_parser;
+use my_parser_striletska::list_parser;
 pub fn main() {
       assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
       println!("{:?}", list_parser::list("[1,1,2,3,5,8]"));
